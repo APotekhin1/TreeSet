69cff321068aa20ca569295f12a7eefb894f3b15
APotekhin1 <alex01210@gmail.com>
2022-09-23 09:24:45 +0300

    пробуем сделать Лямбда-метод

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,15 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = generetePerson();
-        PersonComparator personComparator = new PersonComparator(1);
+
+        //PersonComparator personComparator = new PersonComparator(1);
         System.out.println("До сортировки \n"
                 + people.stream().map(Person::toString).collect(Collectors.joining("\n")));
-        people.sort(personComparator);
+        //people.sort(personComparator);
+        (o1, o2) -> {
+
+            return compare(o1, o2);
+        };
         System.out.println("\n После сортировки \n"
                 + people.stream().map(Person::toString).collect(Collectors.joining("\n")));
     }
@@ -20,5 +25,13 @@ public class Main {
                 new Person("Far", "Al Gazi Bula", 40),
                 new Person("Radg", "Ali", 25)
         ).collect(Collectors.toList());
+    }
+    public static int compare(Person o1, Person o2) {
+        int lengthSureNameFirstPerson = o1.getSurName().split("\\h").length;
+        int lengthSureNameSecondPerson = o2.getSurName().split("\\h").length;
+        if (lengthSureNameFirstPerson == lengthSureNameSecondPerson) {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+        return lengthSureNameFirstPerson > lengthSureNameSecondPerson ? 1 : -1;
     }
 }
